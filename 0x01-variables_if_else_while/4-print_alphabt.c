b42816525cd346d024e4e7d4b0905fe5adbfe13a
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-17 09:59:30 -0700

    added task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase except q and e
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
