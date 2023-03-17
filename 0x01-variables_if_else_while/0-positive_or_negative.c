52c4a5a646569ad889d203e89d286a2ab68539ce
Anas Bahraoui <80850613+Anas127@users.noreply.github.com>
2023-03-17 16:50:07 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
-* main-prints a number and checks if it is negative or positive or equals a zero
+* main-prints a number checks if it is negative or positive or equals a zero
 * Return: always 0
 */
 int main(void)
