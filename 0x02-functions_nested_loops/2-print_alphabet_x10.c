2bcf1b8dd3c01f21f6ce6a633b82db07269741db
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-21 02:07:59 -0700

    fixed task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char l;
 	int i;
 
-	for (l = 'a'; l <= 'z'; l++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 9; i++)
+		for (l = 'a'; l <= 'z'; l++)
 			_putchar(l);
 		_putchar('\n');
 	}
