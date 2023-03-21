1994c9be992046ba347225b36c170356e8b5baa3
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-21 03:05:54 -0700

    added task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @n: number to be tested
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
