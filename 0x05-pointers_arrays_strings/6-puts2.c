57047be7cc4c236494f29d3733c85bdade0395bc
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-27 14:14:57 -0700

    fixed task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - prints every character
+ * @str: char
+ */
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
+}
