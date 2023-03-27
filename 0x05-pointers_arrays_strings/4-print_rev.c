20c016a0d096fe7950dfbabbee52d3f739e5b4ac
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-27 14:01:54 -0700

    fixed task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,21 @@
-#include "main.h" 
-  
- /** 
- * print_rev - printing a string in reverse 
- * @s: the string
- */ 
-  
- void print_rev(char *s) 
- { 
-         int i, n; 
-  
-         n = 0; 
-         while (s[n] != '\0') 
-                 n++; 
-  
-         for (i = n - 1; i >= 0; i--) 
-         { 
-                 _putchar(s[i]); 
-         } 
-         _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+ * print_rev - print string in rev
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
