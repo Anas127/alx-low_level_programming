c977dd1ca8269a8920b219f293dc498bc213e60b
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-27 14:21:19 -0700

    fixed task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _puts - prints a string
- * @str: string parameter to print
- * Return: Nothing
-*/
-
+ * _puts - prints string
+ * @str: string
+ */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(*str + 0);
-		++str;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
