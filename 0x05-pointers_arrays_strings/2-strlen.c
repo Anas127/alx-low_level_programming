c954f5ade83e33045ac03dd360a010507f6cf2ed
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-27 07:06:42 -0700

    fixed task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,17 +10,9 @@
 int _strlen(char *s)
 {
 	int counter;
-	
-<<<<<<< HEAD
-	for (counter =  0; *s  != '\0'; s++)
-		counter++;
-=======
-	for (counter = 0; *s != '\0'; s++)
-	{
-		counter++;
-	}
->>>>>>> 54411d27a2776a726cd668f00e7eebcef785e44f
+
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
 
 	return (counter);
-	
 }
