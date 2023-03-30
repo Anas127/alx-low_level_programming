a73f2926d98c15e58acc3632b72e62d74a1b9dec
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-29 18:44:56 -0700

    update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,4 @@
+#include <string.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *strcat(char *dest, char *src, int n);
-char *strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
