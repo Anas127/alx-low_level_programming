c36d768a234c7d04fd6bc4ca91d6f7140ebdd28d
Bahraoui Anas <bahraouianas01@gmail.com>
2023-03-29 18:59:25 -0700

    added task 1 fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	char *string = dest;
 
-	while (*string != '\0')
-		string++;
-	int i = 0;
+	while (*dest != '\0')
+		dest++;
+	size_t i = 0;
 
 	while (i < n && src[i] != '\0')
 	{
-		*(string + i) = *(src + i);
+		*(dest + i) = *(src + i);
 		i++;
 	}
-	*(string + i) = '\0';
+	*(dest + i) = '\0';
 
-	return (dest);
+	return (string);
 }
