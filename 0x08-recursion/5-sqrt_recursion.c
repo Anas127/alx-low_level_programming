2e5eefdff419867867adff36e78b70bc20be97b5
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-05 13:40:15 -0700

    added task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return square root
+ * @n: number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	int i = 1;
+
+	while (i * i <= n)
+		i++;
+	return (_sqrt_recursion(n - i + 1) + (i - 1));
+}
