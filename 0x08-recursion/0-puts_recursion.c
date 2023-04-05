adf4006c7a12862d631d7be1a054fb086c844e41
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-04 21:54:08 -0700

    task 0 updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,16 @@
 /**
  * _puts_recursion - prints a string
  * @s: string
+ * Return: ends program
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
