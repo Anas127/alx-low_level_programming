d5a53dc08b84249558ac7b36cd3a435b967005de
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-08 13:56:21 -0700

    fixed task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argc[])
+
+/**
+ * main - prints name program
+ * @argc: int
+ * @argv: array
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
