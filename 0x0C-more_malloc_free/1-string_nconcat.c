c0c6b9ec8292465a5e81bb7f14d07918b9b833b2
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-13 17:20:59 -0700

    added task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,4 +8,42 @@
  * Return: pointer
  */
 
+ar *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int len;
+	char *result;
+	unsigned int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2] && len2 < n)
+		len2++;
+	len = len1 + len2;
+	result = malloc(len + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
 
