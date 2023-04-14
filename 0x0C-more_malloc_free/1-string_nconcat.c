0e7bb844d58b8af732ad2e6ca14b05060f121d3b
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-13 17:33:09 -0700

    fixed function name

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  * Return: pointer
  */
 
-ar *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1;
 	unsigned int len2;
