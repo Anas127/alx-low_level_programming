3e3bcc425a8d736d0f9d70b94cdc829c0e4551f0
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-17 01:53:56 -0700

    adding all tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file compiled
+ * Return : 0 if success
+ */
+
+int main(void){
+	printf("The program was compiled from: %s\n", __FILE__);
+	return (0);
+}
