ae7b155bab2c1d81f20bc083c2851c6a866b09b0
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-19 18:32:35 -0700

    added task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @array: param
+ * @size: param
+ * @cmp: param
+ * Return: -1 if fail, index otherwise
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		if (array || cmp)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
+		}
+	}
+	return (-1);
+}
