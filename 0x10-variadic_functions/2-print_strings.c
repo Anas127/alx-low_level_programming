dc134e7b1a8be2d886cd3562725aa9f053dbde69
Bahraoui Anas <bahraouianas01@gmail.com>
2023-04-20 19:38:01 -0700

    added task 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: param
+ * @n: param
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	char *string;
+
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(a, char *);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
+}
